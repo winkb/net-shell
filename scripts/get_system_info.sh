102f933a2c67c559617472944651414c94c306e2
Your Name <you@example.com>
2025-11-01 13:53:25 +0800

    update

--- scripts/get_system_info.sh
+++ scripts/get_system_info.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 set -e
 
 get_system
--- scripts/global.sh
+++ scripts/global.sh
@@ -1,9 +1,14 @@
 function get_system(){
-echo "Getting system information..."
-echo "OS Version: $(uname -s) $(uname -r)"
-echo "Hostname: $(hostname)"
-echo "Current user: $(whoami)"
-echo "System uptime: $(uptime)" 
-echo "foo== {{ foo }}"
+    echo "Getting system information..."
+    echo "OS Version: $(uname -s) $(uname -r)"
+    echo "Hostname: $(hostname)"
+    echo "Current user: $(whoami)"
+    echo "System uptime: $(uptime)" 
+    echo "foo== {{ foo }}"
 }
+
 echo "global script"
+
+function go(){
+    echo "go function in global script"
+}
